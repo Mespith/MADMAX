d67c656355698feb823b3fea0c000ac5d6adff7b
Jaimy <jaimyTvandijk@gmail.com>
2015-11-30 12:23:37 +0100

    Created the method to assign individuals to species.

--- src/NodeGene.java
+++ src/NodeGene.java
@@ -3,8 +3,20 @@
  */
 public class NodeGene {
 
-    private Double[] inputs;
-    private Double[] hidden;
-    private Double[] outputs;
+    public enum Type {
+        None,
+        Input,
+        Hidden,
+        Output
+    }
 
+    public String name;
+    public Type type = Type.Hidden;
+
+    NodeGene(String name, Type type) {
+        this.name = name;
+        if (type != Type.None) {
+            this.type = type;
+        }
+    }
 }
--- src/Population.java
+++ src/Population.java
@@ -1,11 +1,41 @@
+import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Jaimy on 26/11/2015.
  */
 public class Population {
 
     private Double c1, c2, c3;
+    private Double compatibility_threshold;
+    private List<Genome> Species;
+    private List<Genome> Generation;
 
     // (Re)Assign species
+    public ArrayList<ArrayList<Genome>> Spieciefy() {
+        ArrayList<ArrayList<Genome>> generation_species = new ArrayList<>();
+        Boolean added = false;
+        // Loop through all the individuals of this generation.
+        for (int i = 0; i < Generation.size(); i++) {
+            Genome individual = Generation.get(i);
+            // Loop through all the known species.
+            for (int j = 0; j < Species.size(); j++) {
+                Double comp = compatibility(individual, Species.get(j));
+                // If the individual is compatible with the species, it is assigned to it.
+                if (comp < compatibility_threshold) {
+                    generation_species.get(j).add(individual);
+                    added = true;
+                    break;
+                }
+            }
+            // If the individual was nog assigned to any species, it is a new species.
+            if (!added) {
+                Species.add(individual);
+            }
+        }
+        return generation_species;
+    }
 
     // Kill the worst performing individuals of each species
 
