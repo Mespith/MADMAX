87b711c028fec05e07bd423f1f2c712e132d8270
kingkongkai <iakai.no@gmail.com>
2015-12-01 14:49:02 +0100

    Update NodeGene.java

--- src/NodeGene.java
+++ src/NodeGene.java
@@ -9,16 +9,22 @@ public class NodeGene {
         Hidden,
         Output
     }
-
+    
     public Integer id = 0;
     public Type type = Type.Hidden;
+    
+    public ConnectionGene[] connections;
     
     NodeGene(NodeGene g){
         this.type = g.type;
         this.id = g.id;
+        ConnectionGene[] = new ConnectionGene[g.connections.size];
+        for (int i = 0; i < g.connections.size; i++){
+            connections[i] = g.connections[i];
+        }
     }
 
-    NodeGene(Integer id, Type type) {
+    NodeGene(Integer id, Type type, ConnectionGene[] connections) {
         if (type != Type.None) {
             this.type = type;
         }
@@ -26,5 +32,7 @@ public class NodeGene {
         if (type == Type.Input || type == Type.Output) {
             this.id = id;
         }
+        
+        this.connections = connections;
     }
 }
