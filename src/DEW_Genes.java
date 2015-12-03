772d631b4d2d2c79b15564d0ba6506fa4cf3a606
kingkongkai <iakai.no@gmail.com>
2015-12-03 23:30:21 +0100

    Update DEW_Genes.java

--- src/DEW_Genes.java
+++ src/DEW_Genes.java
@@ -1,52 +1,43 @@
-/**
- * Created by Jaimy on 30/11/2015.
- */
 public class DEW_Genes {
-    private int E;
-    private int D;
-    private double W;
-    private int N;
+    public int E;
+    public int D;
+    public double W;
+    public int N;
 
     DEW_Genes(Genome g1, Genome g2) {
         E = 0;
         N = 0; //# of shared genes
         W = 0.;
 
-        int i1 = g1.Genes()[0].getInnovation_nr();
-        int i2 = g2.Genes()[0].getInnovation_nr();
+        int i1 = g1.genes.get(0).getInnovation_nr();
+        int i2 = g2.genes.get(0).getInnovation_nr();
 
         while (i1 == i2 && N < g1.N && N < g2.N) {
-            W += Math.abs(g1.Genes()[N].getWeight() - g2.Genes()[N].getWeight());
+            W += Math.abs(g1.genes.get(N).weight() - g2.genes.get(N).weight());
             N++;
             if (N < g1.N && N < g2.N) {
-                i1 = g1.Genes()[N].getInnovation_nr();
-                i2 = g2.Genes()[N].getInnovation_nr();
+                i1 = g1.genes.get(N).getInnovation_nr();
+                i2 = g2.genes.get(N).getInnovation_nr();
             }
         }
 
         W /= N;
 
-        i1 = g1.Genes()[g1.N - 1].getInnovation_nr();
-        i2 = g2.Genes()[g2.N - 1].getInnovation_nr();
+        i1 = g1.genes.get(g1.N - 1).getInnovation_nr();
+        i2 = g2.genes.get(g2.N - 1).getInnovation_nr();
 
         if (i1 < i2) {
             while (i1 < i2) {
                 E++;
-                i2 = g2.Genes()[g2.N - 1 - E].getInnovation_nr();
+                i2 = g2.genes.get(g2.N - 1 - E).getInnovation_nr();
             }
         } else if (i2 < i1) {
             while (i2 < i1) {
                 E++;
-                i1 = g1.Genes()[g1.N - 1 - E].getInnovation_nr();
+                i1 = g1.genes.get(g1.N - 1 - E).getInnovation_nr();
             }
         }
 
         D = g1.N + g2.N - 2 * N - E;
     }
-
-    public int getE(){ return E; }
-    public int getD(){ return D; }
-    public int getN(){ return N;}
-    public double getW(){ return W; }
-
 }
