de9c9e9eca72c6030cbc23557bbf62713d598001
Jaimy T. van Dijk <jaimyvandijk@hotmail.com>
2015-12-03 20:10:06 +0100

    There was still something wrong in de Neural Network class, but it is fixed now.

--- src/NeuralNetwork.java
+++ src/NeuralNetwork.java
@@ -9,7 +9,7 @@ public class NeuralNetwork implements Serializable {
 	private Vector<Double> x_prev;
 	private Double leaking_rate;
 
-	NeuralNetwork(Vector<Vector<Double>> W, Vector<Vector<Double>> Win, Vector<Vector<Double>> Wout, Double leaking_rate){
+	NeuralNetwork(Vector<Vector<Double>> W, Vector<Vector<Double>> Win, Vector<Vector<Double>> Wout, Double leaking_rate) {
 		this.W = W;
 		this.Win = Win;
 		this.Wout = Wout;
@@ -37,33 +37,32 @@ public class NeuralNetwork implements Serializable {
 	}
 
 	// Load a neural network from memory
-	 public NeuralNetwork loadGenome(Parser parser) {
+	public NeuralNetwork loadGenome(Parser parser) {
 
-		 // Read from disk using FileInputStream
-		 FileInputStream f_in = null;
-		 try {
-			 f_in = new FileInputStream("memory/madmax.mem");
-		 } catch (FileNotFoundException e) {
-			 e.printStackTrace();
-		 }
+		// Read from disk using FileInputStream
+		FileInputStream f_in = null;
+		try {
+			f_in = new FileInputStream("memory/madmax.mem");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 
-		 // Read object using ObjectInputStream
-		 ObjectInputStream obj_in = null;
-		 try {
-			 obj_in = new ObjectInputStream(f_in);
-		 } catch (IOException e) {
-			 e.printStackTrace();
-		 }
+		// Read object using ObjectInputStream
+		ObjectInputStream obj_in = null;
+		try {
+			obj_in = new ObjectInputStream(f_in);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-		 // Read an object
-		 try {
-			 return (NeuralNetwork) obj_in.readObject();
-		 } catch (IOException e) {
-			 e.printStackTrace();
-		 } catch (ClassNotFoundException e) {
-			 e.printStackTrace();
-		 }
-
-		 return parser.ParseNetworkFile("OutputWeights.txt");
-	 }
+		// Read an object
+		try {
+			return (NeuralNetwork) obj_in.readObject();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
+}
